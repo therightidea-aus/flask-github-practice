18b2f4e461d5a4765e87a0df6d3ef611daefc3a8
Roger Curran <roger@therightidea.com.au>
2026-02-11 21:15:34 +1100

    Update to homepage message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Hello GitHub + Flask 👋"
+    return "Hello GitHub + Flask 👋, we hope you have a good night"
 
 if __name__ == "__main__":
     app.run(debug=True)
